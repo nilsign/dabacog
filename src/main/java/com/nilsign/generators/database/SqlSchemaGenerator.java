f79324d4ffb8cc1224a185174f0c998a540704a7
Nils Heumer <nheumer@gmail.com>
2020-05-27 01:52:06 +0200

    Sql table and field name normalization
    - Normalizes table names

--- src/main/java/com/nilsign/generators/database/SqlSchemaGenerator.java
+++ src/main/java/com/nilsign/generators/database/SqlSchemaGenerator.java
@@ -5,13 +5,16 @@ import com.nilsign.dxd.xml.entities.DxdEntityClass;
 import com.nilsign.misc.Pair;
 import lombok.NonNull;
 
+
 public class SqlSchemaGenerator {
 
   public static final String SQL_PRIMARY_KEY_NAME = "id";
   private static final String SQL_TABLE_PREFIX = "tbl";
 
   public static String buildTableName(@NonNull DxdEntityClass entityClass) {
-    return String.format("%s_%s", SQL_TABLE_PREFIX, entityClass.getName());
+    return String.format("%s_%s",
+        SQL_TABLE_PREFIX,
+        transformClassEntityName(entityClass.getName()));
   }
 
   public static String buildTableName(@NonNull DxdEntityRelation relation) {
@@ -21,16 +24,34 @@ public class SqlSchemaGenerator {
   }
 
   public static String buildForeignKeyName(@NonNull String referencedClassName) {
-    return String.format("%s_%s", SQL_PRIMARY_KEY_NAME, referencedClassName);
+    return String.format("%s_%s",
+        SQL_PRIMARY_KEY_NAME,
+        transformClassEntityName(referencedClassName));
   }
 
   public static Pair<String, String> buildForeignKeyNames(@NonNull DxdEntityRelation relation) {
     return Pair.of(
         String.format("%s_%s",
             SQL_PRIMARY_KEY_NAME,
-            relation.getReferencedClass().getName()),
+            transformClassEntityName(relation.getReferencedClass().getName())),
          String.format("%s_%s",
             SQL_PRIMARY_KEY_NAME,
-            relation.getReferencingClass().getName()));
+             transformClassEntityName(relation.getReferencingClass().getName())));
+  }
+
+  private static String transformClassEntityName(String entityName) {
+    String normalizedName = "";
+    for (int i = 0; i < entityName.length(); ++i) {
+      if (i > 0
+          && Character.isUpperCase(entityName.charAt(i))
+          && Character.isLowerCase(entityName.charAt(i - 1))
+          || i > 1
+          && Character.isLowerCase(entityName.charAt(i))
+          && Character.isUpperCase(entityName.charAt(i - 1))) {
+        normalizedName += "_";
+      }
+      normalizedName += entityName.charAt(i);
+    }
+    return normalizedName.toLowerCase();
   }
 }
