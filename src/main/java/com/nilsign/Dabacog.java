445537da973b7f758954d54856f4368f031bf142
Nils Heumer <nheumer@gmail.com>
2020-08-04 01:25:01 +0200

    Dabacog CLI
    - Adds a callable CLI picocli root commant
    - Moves traget generation execution to root command
    - Adds maven-shade-plugin pom to build a fat jar without name clashes
    and an included valid manifest

--- src/main/java/com/nilsign/Dabacog.java
+++ src/main/java/com/nilsign/Dabacog.java
@@ -1,116 +1,37 @@
 package com.nilsign;
 
-import com.nilsign.dxd.DxdModelException;
-import com.nilsign.dxd.XmlToDxdConverter;
-import com.nilsign.dxd.model.DxdModel;
-import com.nilsign.generators.diagrams.database.GraphvizDotRenderer;
-import com.nilsign.generators.diagrams.database.GraphvizDotRendererException;
-import com.nilsign.generators.diagrams.database.dot.DotDatabaseDiagramGenerator;
-import com.nilsign.generators.diagrams.database.dot.DotGeneratorException;
-import com.nilsign.reader.xml.XmlReader;
-import com.nilsign.reader.xml.XmlReaderException;
-import com.nilsign.reader.xml.model.XmlModel;
+import com.nilsign.cli.RootCommand;
 import lombok.AccessLevel;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Arrays;
+import picocli.CommandLine;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Dabacog {
 
-  @NonNull
-  private static final String DABACOG_VERSION = "0.0.1";
+  @NonNull Object command;
+  public static final CommandLine CLI = new CommandLine(new RootCommand());
 
   @NonNull
-  private static final String DXD_FILE_PATH = "./src/main/resources/dev/library.dxd";
+  public static final String DABACOG_VERSION = "0.1.0 alpha";
 
-  @NonNull
-  private static boolean flagDebug = false;
-
-  @NonNull
-  private static boolean flagShowVersion = false;
-
-  @NonNull
-  private static XmlModel xmlModel;
-
-  @NonNull
-  private static DxdModel dxdModel;
-
-  // TODO(nilsheumer): User picocli as framework to create the CLI.
-  // https://github.com/remkop/picocli
   public static void main(@NonNull String[] arguments) {
+    int exitCode = -1;
     try {
-      extractFlagsFromArguments(arguments);
-      if (arguments != null && arguments.length > 0 && arguments[0].equals("-v")) {
-        System.exit(0);;
-      }
-      Dabacog.printDabacog();
-      Dabacog.readXmlFile();
-      Dabacog.buildDxdModel();
-      Dabacog.generateDotDatabaseDiagram();
-      Dabacog.renderDotDatabaseDiagram();
+      CLI.parseArgs(arguments);
+    } catch(Exception e) {
+      System.out.println("ERROR: Dabacog CLI syntax. " + e.getMessage() + ".");
+      System.out.println("Example: dabacog --source ./app.dxd -target sql code --verbose-logging");
+      System.out.println("Try 'dabacog --help' for more information.");
+      System.exit(exitCode);
+    }
+    try {
+      exitCode = ((RootCommand) CLI.getCommand()).call();
     } catch (Exception e) {
-      if (flagDebug) {
-        e.printStackTrace();
-      } else {
-        System.out.println(e.getMessage());
-      };
-      System.exit(-1);
+      System.out.println(e.getMessage());
+    } finally {
+      System.exit(exitCode);
     }
   }
+ }
 
-  private static void extractFlagsFromArguments(@NonNull String[] arguments) {
-    flagShowVersion = extractFlagFromArguments(arguments, "-v", "--version");
-    flagDebug = extractFlagFromArguments(arguments, "-d", "--debug");
-  }
-
-  private static boolean extractFlagFromArguments(
-      @NonNull String[] arguments,
-      @NonNull String shortArgument,
-      @NonNull String longArgument) {
-    return Arrays.stream(arguments).anyMatch(argument
-        -> argument.equalsIgnoreCase(shortArgument) || argument.equalsIgnoreCase(longArgument));
-  }
-
-  private static void printDabacog() {
-    System.out.println("    ____        __");
-    System.out.println("   / __ \\____ _/ /_  ____ __________  ____ _");
-    System.out.println("  / / / / __ `/ __ \\/ __ `/ ___/ __ \\/ __ `/");
-    System.out.println(" / /_/ / /_/ / /_/ / /_/ / /__/ /_/ / /_/ /");
-    System.out.println("/_____/\\__,_/_,___/\\__,_/\\___/\\____/\\__, /");
-    System.out.println("                                   /____/");
-    System.out.println(String.format("Version %s - development", DABACOG_VERSION));
-    System.out.println();
-  }
-
-  private static void readXmlFile() throws XmlReaderException {
-    System.out.println(String.format("Parsing Dxd file '%s'...", DXD_FILE_PATH));
-    xmlModel = XmlReader.run(Dabacog.DXD_FILE_PATH);
-    System.out.println(String.format("Parsing Dxd file -> [DONE]", DXD_FILE_PATH)) ;
-    if (flagDebug) {
-      System.out.print(xmlModel.toString());
-    }
-  }
-
-  private static void buildDxdModel() throws DxdModelException {
-    System.out.println(String.format("Preparing Dxd Model..."));
-    dxdModel = XmlToDxdConverter.run(xmlModel);
-    System.out.println(String.format("Preparing Dxd Model -> [DONE]"));
-    if (flagDebug) {
-      System.out.print(dxdModel.toString());
-    }
-  }
-
-  private static void generateDotDatabaseDiagram() throws DotGeneratorException {
-    System.out.println(String.format("Generating database diagram description..."));
-    DotDatabaseDiagramGenerator.run(dxdModel);
-    System.out.println(String.format("Generating database diagram description -> [DONE]"));
-  }
-
-  public static void renderDotDatabaseDiagram() throws GraphvizDotRendererException {
-    System.out.println(String.format("Rendering database diagram..."));
-    GraphvizDotRenderer.run(dxdModel);
-    System.out.println(String.format("Rendering database diagram -> [DONE]"));
-  }
-}
--- src/main/java/com/nilsign/reader/xml/XmlReaderException.java
+++ src/main/java/com/nilsign/reader/xml/XmlReaderException.java
@@ -4,7 +4,7 @@ import lombok.NonNull;
 
 public final class XmlReaderException extends RuntimeException {
 
-  private static final String ERROR_CLASS_MESSAGE = "Failed to read the input Xml file.";
+  private static final String ERROR_CLASS_MESSAGE = "Failed to read the input Dxd file.";
 
   public XmlReaderException(@NonNull String message, @NonNull Exception e) {
     super(String.format("%s %s", ERROR_CLASS_MESSAGE, message), e);
