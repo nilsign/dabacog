ad246b04f7c337c1dec4510f372279d11ec37df3
Nils Heumer <nheumer@gmail.com>
2020-10-28 01:42:15 +0100

    Generate Java entities
    - Adds entity field gettter and setter methodes.

--- src/main/java/com/nilsign/generators/code/java/Java.java
+++ src/main/java/com/nilsign/generators/code/java/Java.java
@@ -1,7 +1,5 @@
 package com.nilsign.generators.code.java;
 
-import com.nilsign.dxd.model.DxdClass;
-import com.nilsign.dxd.model.DxdField;
 import com.nilsign.generators.Generator;
 import lombok.AccessLevel;
 import lombok.NonNull;
@@ -18,75 +16,15 @@ public final class Java {
     return String.format("// %s", Generator.buildGeneratedByComment());
   }
 
-  public String addPackage(@NonNull String name) {
-    return String.format("\npackage %s;\n", name);
-  }
-
-  public String addImport(@NonNull String path) {
-    return String.format("\nimport %s;", path);
-  }
-
-  public static String openClass(@NonNull DxdClass aClass) {
-    return openClass(normalizeClassName(aClass.getName()));
-  }
-
-  public static String openClass(@NonNull String name) {
-    return String.format("\npublic class %s {", name);
-  }
-
-  public static String declareField(@NonNull DxdField field) {
-    return String.format(
-        "\n  private %s %s;\n",
-        buildJavaType(field),
-        normalizeFieldName(field.getName()));
-  }
-
-  public static String addGetter(@NonNull DxdField field) {
-    return new StringBuffer()
-        .append(String.format(
-            "\n  public %s get%s() {",
-            buildJavaType(field),
-            startUpperCased(field.getName())))
-        .append(String.format(
-            "\n    return %s;",
-            normalizeFieldName(field.getName())))
-        .append("\n}\n")
-        .toString();
-  }
-
-  public static String closeClass() {
-    return "\n}\n";
-  }
-
-  public static String buildJavaType(@NonNull DxdField field) {
-      String dataType =
-          field.getType().isObject() ? field.getType().getObjectName() :
-          field.getType().isString() ? "String" :
-          field.getType().isLong() ? "long" :
-          field.getType().isInt() ? "int" :
-          field.getType().isDouble() ? "double" :
-          field.getType().isFloat() ? "float" :
-          field.getType().isBoolean() ? "boolean" :
-          field.getType().isDate() ? "Date" :
-          field.getType().isBlob() ? "byte[]" :
-          null;
-      if (dataType == null) {
-        throw new RuntimeException(String.format(
-            "Unsupported Java datatype '%s'.",
-            field.getType().getName()));
-      }
-      return dataType;
-  }
-
   public static String normalizeClassName(@NonNull String name) {
     return startUpperCased(name);
   }
 
-  private static String normalizeFieldName(@NonNull String name) {
+  public static String normalizeFieldName(@NonNull String name) {
     return String.format("%s%s", name.substring(0, 1).toLowerCase(), name.substring(1));
   }
 
-  private static String startUpperCased(@NonNull String name) {
+  public static String startUpperCased(@NonNull String name) {
     return String.format("%s%s", name.substring(0, 1).toUpperCase(), name.substring(1));
   }
 }
--- src/main/java/com/nilsign/generators/code/java/entities/JavaEntityClassBuilder.java
+++ src/main/java/com/nilsign/generators/code/java/entities/JavaEntityClassBuilder.java
@@ -7,6 +7,7 @@ import com.nilsign.generators.code.java.Java;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 import lombok.NonNull;
@@ -15,12 +16,14 @@ import javax.lang.model.element.Modifier;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class JavaEntityClassBuilder {
 
   public static String buildEntityClass(@NonNull DxdModel model, @NonNull DxdClass aClass) {
     TypeSpec javaClass = TypeSpec.classBuilder(Java.normalizeClassName(aClass.getName()))
         .addFields(buildFieldSpecs(aClass.getFields()))
+        .addMethods(buildMethodSpecs(aClass.getFields()))
         .build();
     return JavaFile
         .builder(model.getConfig().getCodePackageName(), javaClass)
@@ -29,16 +32,40 @@ public final class JavaEntityClassBuilder {
   }
 
   private static List<FieldSpec> buildFieldSpecs(@NonNull List<DxdField> fields) {
-    List<FieldSpec> fieldSpecs = new ArrayList<>();
-    fields.forEach(field -> fieldSpecs.add(buildFieldSpec(field)));
-    return fieldSpecs;
+    return fields.stream().map(field
+        -> FieldSpec
+            .builder(
+                field.getType().isObject()
+                    ? ClassName.get("com.nilsign.dabacog.demo", field.getType().getObjectName())
+                    : getJavaTypeName(field),
+                Java.normalizeFieldName(field.getName()),
+                Modifier.PRIVATE)
+            .build())
+        .collect(Collectors.toList());
   }
 
-  private static FieldSpec buildFieldSpec(@NonNull DxdField field) {
-    return
-        FieldSpec.builder(field.getType().isObject()
-            ? ClassName.get("com.nilsign.dabacog.demo", field.getType().getObjectName())
-            : getJavaTypeName(field), field.getName(), Modifier.PRIVATE)
+  private static List<MethodSpec> buildMethodSpecs(@NonNull List<DxdField> fields) {
+    List<MethodSpec> methodSpecs = new ArrayList<>();
+    fields.forEach(field -> methodSpecs.addAll(List.of(
+        buildGetterMethodSpec(field),
+        buildSetterMethodSpec(field))));
+    return methodSpecs;
+  }
+
+  private static MethodSpec buildGetterMethodSpec(@NonNull DxdField field) {
+    return MethodSpec.methodBuilder(String.format("get%s", field.getName()))
+        .addModifiers(Modifier.PUBLIC)
+        .addStatement(String.format("return %s", Java.normalizeFieldName(field.getName())))
+        .returns(getJavaTypeName(field))
+        .build();
+  }
+
+  private static MethodSpec buildSetterMethodSpec(@NonNull DxdField field) {
+    String fieldName = Java.normalizeFieldName(field.getName());
+    return MethodSpec.methodBuilder(String.format("set%s", field.getName()))
+        .addModifiers(Modifier.PUBLIC)
+        .addParameter(getJavaTypeName(field), fieldName)
+        .addStatement(String.format("this.%s = %s", field.getName(), field.getName()))
         .build();
   }
 
